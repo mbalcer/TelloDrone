57345fb4bb5c87f42e324026338f8ed939c4e6c5
mbalcer <mateusz75319@gmail.com>
2021-12-07 17:17:29 +0100

    Add drone class and simple choreography

---
+++ src/main/java/Drone.java
@@ -0,0 +1,77 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Drone {
+    private final int udpPort = 8889;
+    private String ip;
+    private DatagramSocket socket;
+    private InetAddress IPAddress;
+    private boolean isConnected = false;
+
+    public Drone(String ip) {
+        this.ip = ip;
+    }
+
+    public boolean is_ok() {
+        String receiveMessage = receiveMessage();
+        System.out.println("Receive message: " + receiveMessage);
+        return receiveMessage.contains("ok");
+    }
+
+    public boolean connect() {
+        try {
+            System.out.println("Connecting to drone");
+            IPAddress = InetAddress.getByName(ip);
+            socket = new DatagramSocket(udpPort);
+            isConnected = sendMessage("command");
+            if (isConnected) {
+                System.out.println("Successfully connected to the drone");
+                return true;
+            } else {
+                System.out.println("Cannot connect to the drone");
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void sendCommand(String command) {
+        System.out.println("Command: " + command);
+        sendMessage(command);
+        try {
+            Thread.sleep(5000);
+            System.out.println("Waiting 5 sec...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Received message: " + receiveMessage());
+        System.out.println(command + " complete");
+    }
+
+    private boolean sendMessage(String command) {
+        try {
+            byte[] sendData = command.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, udpPort);
+            socket.send(sendPacket);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private String receiveMessage() {
+        byte[] receiveData = new byte[5];
+        DatagramPacket packet = new DatagramPacket(receiveData, receiveData.length);
+        try {
+            socket.receive(packet);
+        } catch (IOException e) {
+            return "communication error";
+        }
+        return new String(packet.getData());
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Drone tello = new Drone("192.168.0.52");
+
+        tello.connect();
+        tello.sendCommand("takeoff");
+        tello.sendCommand("forward 100");
+        tello.sendCommand("cw 90");
+        tello.sendCommand("forward 100");
+        tello.sendCommand("land");
     }
 }
